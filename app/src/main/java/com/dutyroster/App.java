350a1d6ff0360f228761237fc46bc7c3686094b3
Anino1996 <anino1996.ao@gmail.com>
2021-09-03 12:31:16 -0400

    Updated and ran to confirm

--- app/src/main/java/com/dutyroster/App.java
+++ app/src/main/java/com/dutyroster/App.java
@@ -1,22 +1,20 @@
+package com.dutyroster;
+
 import java.sql.*;
-public class App 
-{
-    public static void main( String[] args )
-    {
-       Connection c =null;
 
-       try{
-            //Class.forName("org.sqlite.JDBC");
-           String path = "/Users/mohammedabdulai/Documents/Project/dutyRosterJava/app/src/main/App.db";
-           c = DriverManager.getConnection(path);
+public class App {
+    public static void main(String[] args) {
+        Connection c = null;
 
-           System.out.print("successful");
-       }
-       catch(Exception e){
+        try {
+            Class.forName("org.sqlite.JDBC");
+            String path = "jdbc:sqlite:/Users/anino1996/Java/dutyRoster/test.db";
+            c = DriverManager.getConnection(path);
+
+            System.out.print("successful");
+        } catch (Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
-
-       }
-       
+        }
     }
 }
